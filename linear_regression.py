2906cf17ae4e7d24558fa43adb5d2d8a3bb345a9
Soumya Kanti Saha <sks19ms060@iiserkol.ac.in>
2021-07-26 12:04:40 +0530

    added cost and hypothesis

--- linear_regression.py
+++ linear_regression.py
@@ -1,1 +1,15 @@
+from typing import Text
 import numpy as np
+
+
+def h(x): # hypothesis function
+    theta_0=0
+    theta_1=1
+    return theta_0+x*theta_1
+
+def cost(data):
+    x=data[:,0].flatten()
+    y=data[:,1].flatten()
+    m=len(x) # size of training set
+    return (np.sum((h(x)-y)**2))*(1/(2*m))
+
